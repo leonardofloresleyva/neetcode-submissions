35e7988acae5b0229aa48e93760ce61d200be608
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2026-06-14 16:56:54 -0700

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        if len(nums) > 1:
+            nums.sort()
+            res = nums[0]
+            count = 1
+            for i in range(1, len(nums)):
+                if nums[i - 1] == nums[i]:
+                    count += 1
+                    if count > len(nums) / 2:
+                        res = nums[i]
+                        break
+                else:
+                    count = 1
+            return res
+        else:
+            return nums[0]
