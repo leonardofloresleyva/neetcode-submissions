c777830c8cd68b1ad753f4981b83e38e4a9e85cd
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2026-07-16 19:33:52 -0700

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    # Hint 3 solution
+    def encode(self, strs: List[str]) -> str:
+        solution = ""
+        for s in strs:
+            l = len(s)
+            solution += f"{l}%{s}"
+        return solution
+
+    def decode(self, s: str) -> List[str]:
+        # Array solution, two pointers, and input string delimiter
+        strList, i, j, end = [], 0, 0, len(s)
+        # Looping through the full string
+        while i < end:
+            if s[j] == "%":
+                left = j + 1
+                length = int(s[i:j])
+                right = left + length
+                strList.append(s[left:right])
+                i = j = right
+                continue
+            j += 1
+        return strList
